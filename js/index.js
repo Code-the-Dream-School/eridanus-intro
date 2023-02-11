f95b4d45857e40cd80a485d73e19e33de8c0169d
Alzhan-B <alzhan.braliyev@gmail.com>
2023-02-11 10:11:25 -0500

    lesson_4.3

--- js/index.js
+++ js/index.js
@@ -15,3 +15,50 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.getElementsByName("leave_message");
+messageForm.item(0).addEventListener("submit", (event) => {  // messageForm. child item?
+    event.preventDefault();  //?
+    const nameInput = event.target.name.value; //?
+    const emailInput = event.target.email.value;
+    const messageInput = event.target.message.value;
+    console.log(nameInput, emailInput, messageInput); 
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${emailInput}">${nameInput}</a> <span> ${messageInput} </span>`;
+    messageList.appendChild(newMessage);
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function() {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    // const editButton = document.createElement("button");
+    // editButton.innerText = "edit";
+    // editButton.type = "button";
+    // editButton.addEventListener("click", function() {
+    //     const entry = this.parentNode;
+    //     const message = prompt("Enter your new message:");
+    //     entry.innerText = newMessage;
+    //     nameInput.contentEditable = true;
+    //     messageInput.contentEditable = true;
+    //     emailInput.contentEditable = true;
+    // })
+    // newMessage.appendChild(editButton);
+    // messageList.appendChild(newMessage);
+
+    // const messagesHeader = messageSection.getElementsByTagName("h2");
+    // if (!messageList.hasChildNodes()) {
+    //     messageSection.style.display = "none";
+    //     messagesHeader.style.display = "none";
+    // }
+    
+    document.querySelector('form').reset();
+});
