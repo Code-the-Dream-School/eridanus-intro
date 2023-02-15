a959d3cd551c7c8b9b8dfff5035f79b7071587c1
Alzhan-B <alzhan.braliyev@gmail.com>
2023-02-14 19:05:28 -0500

    updated version with Edit button

--- js/index.js
+++ js/index.js
@@ -15,11 +15,11 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
-
+// Messages form
 const messageForm = document.getElementsByName("leave_message");
-messageForm.item(0).addEventListener("submit", (event) => {  // messageForm. child item?
-    event.preventDefault();  //?
-    const nameInput = event.target.name.value; //?
+messageForm.item(0).addEventListener("submit", (event) => { 
+    event.preventDefault();  
+    const nameInput = event.target.name.value; 
     const emailInput = event.target.email.value;
     const messageInput = event.target.message.value;
     console.log(nameInput, emailInput, messageInput); 
@@ -30,6 +30,7 @@ messageForm.item(0).addEventListener("submit", (event) => {  // messageForm. chi
     newMessage.innerHTML = `<a href="mailto:${emailInput}">${nameInput}</a> <span> ${messageInput} </span>`;
     messageList.appendChild(newMessage);
 
+    //Remove button
     const removeButton = document.createElement("button");
     removeButton.innerText = "remove";
     removeButton.type = "button";
@@ -40,25 +41,24 @@ messageForm.item(0).addEventListener("submit", (event) => {  // messageForm. chi
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
+    //Edit button
     const editButton = document.createElement("button");
     editButton.innerText = "edit";
     editButton.type = "button";
-    editButton.addEventListener("click", (event) => {
-        // const editEntry = this.parentNode;
-        // const editEntry = event.target.messageInput;
-        console.log("this.parentNode is" + event[0]);
-        const newMessage = prompt("Enter your new message:");
-        // messageInput = newMessage;
-        // console.log(editEntry);
-        
+    editButton.addEventListener("click", () => {
+        let editMessage = prompt("Enter your new message:", `${messageInput}`);
+        newMessage.innerHTML = `<a href="mailto:${emailInput}">${nameInput}</a> <span> ${editMessage} </span>`;
+        newMessage.appendChild(removeButton);
+        newMessage.appendChild(editButton);
     })
     newMessage.appendChild(editButton);
     messageList.appendChild(newMessage);
 
     // const messagesHeader = messageSection.getElementsByTagName("h2");
-    // if (!messageList.hasChildNodes()) {
+    // if (!messages.hasChildNodes()) {
     //     messageSection.style.display = "none";
     //     messagesHeader.style.display = "none";
+    //     document.getElementById("messages").style.display = "none";
     // }
     
     document.querySelector('form').reset();
