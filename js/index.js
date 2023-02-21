e329908d627da7a7da61f31bfec745aef82d67af
tatkh21 <tatkh21@gmail.com>
2023-02-20 23:37:27 -0600

    added remove button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-//Creating a Date object
+//Lesson-4-1 Creating a Date object
+//----------------------------------
 
 const today = new Date();
 const thisYear = today.getFullYear();
@@ -12,16 +13,17 @@ copyright.innerHTML = unicode + " Tatyana " + thisYear;
 footer.appendChild(copyright);
 
 
-// adding list of skills 
+// Lesson-4-2 adding list of skills 
+//----------------------------------
+
  const skills = ["Java", "Java Script", "Type Script", "HTML", "CSS", "SQL"];
  const skillsection = document.getElementById("skills");
- const skillList = document.getElementById("list");
+ const skillList = skillsection.querySelector("ul");
  for (let i = 0; i < skills.length; i++){
     let skill = document.createElement('li');
     skill.innerText = skills[i];
     skillList.appendChild(skill);
  }
-
 
  //Change the colore of the button
  const submitButton = document.getElementById('btn');
@@ -29,56 +31,41 @@ footer.appendChild(copyright);
    submitButton.style.backgroundColor = "blue";
  }
 submitButton.addEventListener("click", changeColore);
-console.log(submitButton.parentNode.parentNode);
 
 
 //Lesson-4-3 selecting form
+//------------------------------
 
 const messageForm = document.getElementById("leave_message_form");
+
  messageForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  const nameValue = messageForm.querySelector('input[type="text"]').value;
-  const emailValue = messageForm.querySelector('input[type="email"]').value;
-  const messageVaue = document.getElementById("field_label_message").value;
-  //creating Message section
+  const nameValue = e.target.name.value;
+  const emailValue = e.target.email.value;
+  const messageValue = e.target.message.value;
+  console.log(nameValue, emailValue, messageValue);
+  messageForm.reset();
+
+ //creating Message section
   const messageSection = document.getElementById("messages");
   const messageList = messageSection.querySelector("ul");
   const newMessage = document.createElement("li");
-  const a = document.createElement("a");
-  a.innerText = nameValue + " wrote: " + newMessage;
-  a.setAttribute("href", emailValue);
+  newMessage.innerHTML = `<a href = mailto:${emailValue}>${nameValue}</a> sent message: <span>${messageValue}</span>`;
   messageList.appendChild(newMessage);
-  newMessage.append(a);
+  //creating remove button
+  const removeButton = document.createElement('button');
+  removeButton.innerText = "Remove";
+  removeButton.setAttribute("type", "button");
+  newMessage.appendChild(removeButton);
 
+  removeButton.addEventListener('click', function(e) {
+    const entry = removeButton.parentNode;
+    console.log(entry);
+    entry.remove('li');
+  })
   
-  messageForm.reset();
- } )
-  // const emailValue = "tatkh21@gmail.com";
-  // const messageSection = document.getElementById("messages");
-  // const messageList = messageSection.querySelector("ul");
-  // const newMessage = document.createElement("li");
-  // const a = document.createElement("a");
-  // a.innerText = "Names";
-  // a.setAttribute("href", emailValue);
-  // messageList.appendChild(newMessage);
-  // newMessage.append(a);
-  // console.log(messageList);
-
+})
  
-// const nameField = document.getElementById("field_label_name");
-// nameField.addEventListener("click", function (e){
-// console.log(e.target.nameField);
-// })
-
-// function formElements(e) {
-//   const nameField = document.getElementById("field_label_name");
-//   nameField.target
-//   const emailField = document.getElementById("field_label_emal");
-//   console.log(emailField)
-//   const messageField = document.getElementById("field_label_message");
-//   console.log(messageField)
-// }
-
 
 
 
@@ -86,15 +73,15 @@ const messageForm = document.getElementById("leave_message_form");
 
 //Dom manipulation practice
 
-//GetElementById()
-const title = document.getElementById('main-heading');
-console.log(title);
-//GetElementByClass()
-const listItems = document.getElementsByClassName('list-items');
-console.log(listItems);
-//GetElementByTagName()
-const tagList = document.getElementsByTagName('li');
-console.log(tagList);
-// querySelector()
+// //GetElementById()
+// const title = document.getElementById('main-heading');
+// console.log(title);
+// //GetElementByClass()
+// const listItems = document.getElementsByClassName('list-items');
+// console.log(listItems);
+// //GetElementByTagName()
+// const tagList = document.getElementsByTagName('li');
+// console.log(tagList);
+// // querySelector()
 
 //querySelectorAll()
