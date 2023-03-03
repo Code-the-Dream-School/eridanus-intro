7c5932ba017174b7a14a837b93002de9752315db
Yulia Lopatko <engineeryulia@gmail.com>
2023-03-02 20:36:45 -0700

    add lesson-4-2

---
+++ index.js
@@ -0,0 +1,26 @@
+const today = new Date();
+const thisYear = today.getFullYear();
+console.log("this is year!", thisYear);
+const footer = document.guerySelector('footer');
+const copyright = document.createElement('p');
+
+copyright.innerHTML = `Yulia Lopatko & copy ${thisYear} &#9816`;
+footer.appendChild(copyright);
+
+const skills= [
+  "HTML",
+  "Node.js",
+  "Customer service",
+  "Busuness Analitics",
+];
+
+const skillsSection = document.getElementById('skills')
+const skillsList = skillsSection.querySelect('ul')
+
+for(let i = 0; i< skills.length; i++){
+  let skill = document.createElement('li')
+  skill.innerText = skills[i]
+  skillsList.appendChild(skill);
+}
+
+
