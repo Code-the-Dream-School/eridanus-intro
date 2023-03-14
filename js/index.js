3fe522a540797396a3d0a893ea8179c3f6981b3f
Olga Adams <83266819+Olga975@users.noreply.github.com>
2023-03-14 13:05:25 -0600

    Lesson 4 3 (#8)
    * added sections
    
    * html form is made
    
    * javascript added for the form
    
    * added change

--- js/index.js
+++ js/index.js
@@ -22,3 +22,37 @@ for (i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
+const messageForm = document.forms.leave_message;
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+
+    newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>says: ${usersMessage}</span>
+`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener("click", (event) => {
+        const entry = event.target.parentNode;
+        entry.remove();
+    })
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+
+});
+
+
