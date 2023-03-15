aa889367c1bfa4a1db8084ee53dfff2abb52d6fc
Kremifany <34751960+Kremifany@users.noreply.github.com>
2023-03-14 22:17:31 -0400

    messages platform added: add and remove message

--- js/index.js
+++ js/index.js
@@ -17,3 +17,44 @@ for(let i=0; i<skills.length; i++){
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.getElementsByName("leave_message")[0];//<--=document.forms.leave_message -->
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const usersName = e.target.usersName.value;//target=form
+    const usersEmail = e.target.usersEmail.value;
+    const usersMessage = e.target.usersMessage.value;
+
+    console.log(usersName, usersEmail, usersMessage);
+
+    const messageSection = document.getElementById("messages");//could be queryselector too
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+   
+
+    newMessage.innerHTML = `
+    <a href = "mailto:${usersEmail}">${usersName}</a>
+    <span>says: ${usersMessage}</span>
+    `;
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", (event) => {
+        const entry = event.target.parentNode;
+        entry.remove();
+        if (messageList.childElementCount === 0) {
+            messageSection.style.display = "none";
+        }
+    });
+
+    messageSection.style.display = "block";
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    
+
+    messageForm.reset();
+});
+    
