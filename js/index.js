6a7ec902e6b4ee418ca1fa6b6f282e1d8edb6d0b
AnaStasiaKlyuch <klucharovanastya@gmail.com>
2023-03-16 23:49:08 -0400

    update

--- js/index.js
+++ js/index.js
@@ -15,10 +15,38 @@ skillsList.appendChild(skill);
 }
 
 const messageForm = document.getElementsByName('leave_message');
-messageForm.addEventlistener("submit", function answer (evt)){
+
+console.log(messageForm);
+
+messageForm[0].addEventListener("submit", function answer(evt){
     evt.preventDefault();
-    const name = evt.target.name;
-    const email = evt.target.email;
-    const message = evt.target.message;
-    console.log(name + email + message
+    const name = evt.target.name.value;
+    const email = evt.target.email.value;
+    const message = evt.target.userMessage.value;
+
+    console.log(name, email, message);
+    
+    const messageSection = document.getElementById("messages");
+    // console.log(messageSection);
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href=mailto:${email}>${name}</a><span> wrote: ${message} </span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    removeButton.setAttribute('type','button');
+    
+        removeButton.addEventListener('click', (event)=>{
+            const entry = event.target.parentNode;
+            console.log(entry);
+            entry.remove();
+            if(messageList.childElementCount === 0){
+                messageSection.style.display = 'none';
+            }
+        });
+        messageSection.style.display = "block";
+        newMessage.appendChild(removeButton);
+        messageList.appendChild(newMessage);
+   
+    messageForm[0].reset();
 });
