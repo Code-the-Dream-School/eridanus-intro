bfcee3e8fb3a5b90361caa15be35718a03432a16
Anton Kondakov <Anakondos@gmail.com>
2023-03-21 19:49:21 -0400

    Revert "lesson 4-3"

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const thisYear = today.getFullYear();
 // console.log(thisYear);
 const footer = document.querySelector('#footer');
 const copyright = document.createElement('p');
-// console.log(copyright);
+console.log(copyright);
 copyright.innerHTML = 'Anton Kondakov &copy ' + thisYear;
 footer.appendChild(copyright); 
 let skills = ['Agile', 'JavaScript', 'HTML', 'CSS' ];
@@ -17,43 +17,3 @@ for(let i = 0; i < skills.length; i++) {
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
-
-const messageForm = document.getElementsByName('leave_message');
-
-// console.log(messageForm);
-
-
-messageForm[0].addEventListener("submit", function answer(evt){
-    evt.preventDefault();
-    
-    const name = evt.target.name.value;
-    const email = evt.target.email.value;
-    const message = evt.target.userMessage.value;
-
-    console.log(name, email, message);
-    
-    const messageSection = document.getElementById("messages");
-    // console.log(messageSection);
-    const messageList = messageSection.querySelector('ul');
-    const newMessage = document.createElement('li');
-    newMessage.innerHTML = `<a href=mailto:${email}>${name}</a><span> wrote: ${message} </span>`;
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'remove';
-    removeButton.setAttribute('type','button');
-    
-        removeButton.addEventListener('click', (event)=>{
-            const entry = event.target.parentNode;
-            console.log(entry);
-            entry.remove();
-            if(messageList.childElementCount === 0){
-                messageSection.style.display = 'none';
-            }
-
-        });
-        messageSection.style.display = "block";
-        newMessage.appendChild(removeButton);
-        messageList.appendChild(newMessage);
-   
-    messageForm[0].reset();
-});
