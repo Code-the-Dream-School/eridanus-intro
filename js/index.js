502f24a7670aabb54a54072a5d8552889cf5b0af
Christie <chrissa03@hotmail.com>
2023-03-27 20:16:53 -0400

    added CSS styling and photo

--- js/index.js
+++ js/index.js
@@ -28,7 +28,6 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
-<<<<<<< HEAD
 // Using "DOM Selection", select the "leave_message" form by name attribute and store it in a variable named messageForm
 
 const messageForm = document.forms.leave_message;
@@ -63,27 +62,6 @@ messageForm.addEventListener('submit', function(event){
 
     messageForm.reset();
 });
-// Append the newMessage to the messageList element
-
-//Append the removeButton to the newMessage element hint: appendChild method
-// Inside the callback function, find the button's parent element using DOM Traversal 
-//(hint: parentNode property) and store it in a variable named entry
-// Remove the entry element from the DOM (hint: remove method)
-
-// On the next line, set the inner HTML of your newMessage element with the following information:
-// <a> element that displays the "name" and links to the "email" (hint: use the mailto: prefix)
-// <span> element that displays the "message"
-
-// Create a new list item (li) element and store it in a variable named newMessage
-// Using "DOM Selection", query the messageSection (instead of the entire document) to find the <ul> element and store it in a 
-// variable named messageList
-// Open index.js and start inside the event listener callback function on the line above where you reset the form
-//  Using "DOM Selection", select the #messages section by id and store it in a variable named messageSection
-// Inside the callback function, above the other code you just wrote, add a new line to prevent the default refreshing behavior of the "submit" event
-// hint: preventDefault method
-// Inside the callback function for your event listener, create a new variable for each of the three form fields and retrieve the value from the event
-// hint: event.target is the form, event.target.usersName is the first input element
 
 
-=======
->>>>>>> main
+
