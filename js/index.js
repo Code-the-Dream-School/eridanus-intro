206a8197bb9d5596eeb61cfd465a7042c6a24b03
Kremifany <34751960+Kremifany@users.noreply.github.com>
2023-03-28 21:31:04 -0400

    navigation bar added

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ const copyright = document.createElement("p");
 copyright.innerHTML = `Fany Kreminski &copy${thisYear}`;
 footer.appendChild(copyright);
 
-const skills = ["HTML","CSS",".NET","C++"];
+const skills = ["HTML","CSS",".NET","C++","JS","C"];
 
 const skillsSection = document.getElementById("skills");
-const skillsList = skillsSection.querySelector("ul");
+const skillsList = skillsSection.querySelector("ul");   
 
 for(let i=0; i<skills.length; i++){
     let skill = document.createElement("li");
