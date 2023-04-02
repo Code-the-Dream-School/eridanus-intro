78be102de7caff64ace63d811146927c18af5b7e
Alzhan-B <alzhan.braliyev@gmail.com>
2023-04-02 14:09:18 -0400

    issues with CSS

--- js/index.js
+++ js/index.js
@@ -6,16 +6,16 @@ const copyright = document.createElement("p");
 copyright.innerHTML = ` Alzhan B &copy ${thisYear}`;
 footer.appendChild(copyright);
 
-const skills = ["Java Script", "HTML", "CSS", "Something else"]; 
+//Putting skills in HTML in ul and li
+const skills = ["Java Script", "HTML", "CSS", "Node.js", "Mocha", "React", "MySQL"]; 
 const skillsSection = document.getElementById("skills");
-
 const skillsList = skillsSection.querySelector("ul");
 for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
-// Messages form
+// Creating Messages form with input fields
 const messageForm = document.getElementsByName("leave_message");
 messageForm.item(0).addEventListener("submit", (event) => { 
     event.preventDefault();  
@@ -27,21 +27,28 @@ messageForm.item(0).addEventListener("submit", (event) => {
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
     const newMessage = document.createElement("li");
+   
     newMessage.innerHTML = `<a href="mailto:${emailInput}">${nameInput}</a> <span> ${messageInput} </span>`;
     messageList.appendChild(newMessage);
 
-    //Remove button
+//Remove button and disappearing Messages section
     const removeButton = document.createElement("button");
     removeButton.innerText = "Remove";
     removeButton.type = "button";
+
     removeButton.addEventListener("click", function() {
         const entry = removeButton.parentNode;
         entry.remove();
+        if (messageList.childElementCount === 0) {
+            messageSection.style.display = "none";
+        }
     });
+
+    messageSection.style.display = "block"
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
-    //Edit button
+//Creating Edit button and new input prompt
     const editButton = document.createElement("button");
     editButton.innerText = "Edit";
     editButton.type = "button";
@@ -53,13 +60,6 @@ messageForm.item(0).addEventListener("submit", (event) => {
     })
     newMessage.appendChild(editButton);
     messageList.appendChild(newMessage);
-
-    // const messagesHeader = messageSection.getElementsByTagName("h2");
-    // if (!messages.hasChildNodes()) {
-    //     messageSection.style.display = "none";
-    //     messagesHeader.style.display = "none";
-    //     document.getElementById("messages").style.display = "none";
-    // }
     
     document.querySelector('form').reset();
 });
