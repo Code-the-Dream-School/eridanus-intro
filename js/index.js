2290d0ec14154b9fb81cac4dd8c735f7a9132b03
Rojina Pradhan <rose1997@Rojinas-MacBook-Air.local>
2023-04-03 14:25:58 -0700

    <added navigation bar and sizing on the website>

--- js/index.js
+++ js/index.js
@@ -14,28 +14,23 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
-let messageSection = document.getElementById("messages");
+const messageForm = document.getElementByName("leave_message")[0];
 messageSection.style.display = "none";
 messageForm.addEventListener('submit', event => {
   event.preventDefault();
   const usersName = event.target.usersName.value;
   const usersEmail = event.target.usersEmail.value;
   const message = event.target.message.value;
-});
-const messageForm = document.forms.leave_message;
 const messageList = messageSection.querySelector('ul');
-  // create a new list item (li) element and store it in a variable named newMessage
-  const newMessage = document.createElement('li');
-  newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> <span>${message}</span>`;
-  const removeButton = document.createElement('button');
-  removeButton.innerText = 'remove';
-  removeButton.type = 'button';
-  removeButton.addEventListener('click', () => {
-    
-    const entry = removeButton.parentNode;
-    entry.remove();
+const newMessage = document.createElement('li');
+newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> <span>${message}</span>`;
+const removeButton = document.createElement('button');
+removeButton.innerText = 'remove';
+removeButton.type = 'button';
+removeButton.addEventListener('click', () => {
+  const entry = removeButton.parentNode;
+  entry.remove();
   });
-  newMessage.appendChild(removeButton);
-  messageList.appendChild(newMessage);
-  messageForm.reset();
-
+newMessage.appendChild(removeButton);
+messageForm.reset();
+});
