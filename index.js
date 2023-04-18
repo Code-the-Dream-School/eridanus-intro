c75ae4f7f1d97577603093ee0bf1181886993dbd
Yulia Lopatko <engineeryulia@gmail.com>
2023-04-18 13:26:04 -0600

    add Lesson_6.2

--- index.js
+++ index.js
@@ -30,45 +30,50 @@ for(let i = 0; i< skills.length; i++){
 
 const messageForm = document.forms.leave_message;
 
-messageForm.addEventListener('submit', (event) => {
+messageForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const userName = event.target.usersName.value;
-    const userEmail = event.target.usersEmail.value;
-    const userMessage = event.target.usersMessage.value;
-    
-    const messageSection = document.getElementById('messages');
-    const messageList = messageSection.querySelector('ul');
-    const newMessage = document.createElement('li');
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    let usersMessage = event.target.usersMessage.value;
 
-    newMessage.innerHTML = `<a href = "mailto:${userEmail}">${userName}</a><span> says: ${userMessage}</span>`;
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    let newMessage = document.createElement("li");
 
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'Remove';
-    removeButton.type = 'button';
+    newMessage.innerHTML = `<a href = "mailto:${usersEmail}">${usersName}</a><span>says: ${usersMessage}</span>`;
 
-    removeButton.addEventListener('click', (event) => {
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", (event) => {
         const entry = event.target.parentNode;
         entry.remove();
     });
+        newMessage.appendChild(removeButton);
+       // newMessage.appendChild(editButton);
+        messageList.appendChild(newMessage);
+        //editForm.remove();
+    //});
 
-    newMessage.appendChild(removeButton);
-    messageList.appendChild(newMessage);
+    //newMessage.appendChild(editButton);
 
     messageForm.reset();
 });
 
 const dateFixer = (date) => {
     return date.slice(0, 10);
-}
-
+} 
 // Method for getiing info from GitHub
-const gitHubRequest = new XMLHttpRequest();
-gitHubRequest.open("GET","https://api.github.com/users/engineeryulia/repos" );
-gitHubRequest.send();
-gitHubRequest.addEventListener('load', () => {
-    const repositories = JSON.parse(gitHubRequest.responseText);
-    console.log(repositories);
+fetch("https://api.github.com/users/engineeryulia/repos")
+.then((response) => {
+if (!response.ok) {
+throw new Error(response.statusText);
+}
+return response.json();
+})
+.then((repositories) => {
 
 // selecting ul in projects section
 const projectSection = document.getElementById('projects');
@@ -76,25 +81,33 @@ const projectList = projectSection.querySelector('ul')
 
 //iterating over repo array to display repo data
 for(let i = 0; i < repositories.length; i++) {
-    const project = document.createElement('li');
+const project = document.createElement('li');
 
-    const projectLink = document.createElement('a');
-    projectLink.innerText = repositories[i].name;
-    projectLink.href = repositories[i].html_url;
-    projectLink.target = "_blank";
+const projectLink = document.createElement('a');
+projectLink.innerText = repositories[i].name;
+projectLink.href = repositories[i].html_url;
+projectLink.target = "_blank";
 
-    const projectDescription = document.createElement('p');
-    projectDescription.innerText = repositories[i].description;
+const projectDescription = document.createElement('p');
+projectDescription.innerText = repositories[i].description;
 
-    const projectDate = document.createElement('p');
-    projectDate.innerText = dateFixer(repositories[i].pushed_at);
+const projectDate = document.createElement('p');
+projectDate.innerText = dateFixer(repositories[i].pushed_at);
 
-    project.appendChild(projectLink);
-    project.appendChild(projectDate);
-    project.appendChild(projectDescription);
-    projectList.appendChild(project);
+project.appendChild(projectLink);
+project.appendChild(projectDate);
+project.appendChild(projectDescription);
+projectList.appendChild(project);
 
-    project.classList.add('projectStyle');
+//styling
+project.classList.add('projectStyle');
 
 }
-});
+})
+.catch((error) => {
+console.warn(error);
+const projectSection = document.getElementById('projects');
+const errorMessage = document.createElement('h1');
+errorMessage.innerText = `There was an error! Github error message: ${error.message}`;
+projectSection.appendChild(errorMessage);
+}); 
